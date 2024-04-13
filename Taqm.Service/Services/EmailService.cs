928bf99f60d720ad4fda09d326fdfd255b932fe9
Abdulrhman Hosny Muhammed <abdulrhmanhosnymuhammed@gmail.com>
2024-04-13 07:09:41 +0200

    Add Email endpoint (#2)

---
+++ Taqm.Service/Services/EmailService.cs
@@ -0,0 +1,54 @@
+﻿using MailKit.Net.Smtp;
+using MimeKit;
+using Taqm.Data.Helpers;
+using Taqm.Service.Abstracts;
+
+namespace Taqm.Service.Services
+{
+    public class EmailService : IEmailService
+    {
+        #region Fields
+        private readonly EmailSettings _emailSettings;
+        #endregion
+
+        #region Constructors
+        public EmailService(EmailSettings emailSettings)
+        {
+            _emailSettings = emailSettings;
+        }
+        #endregion
+
+        #region Methods
+        public async Task<string> SendEmail(string email, string Subject, string message)
+        {
+            try
+            {
+                using (var client = new SmtpClient())
+                {
+                    await client.ConnectAsync(_emailSettings.Host, _emailSettings.Port, true);
+                    client.Authenticate(_emailSettings.FromEmail, _emailSettings.Password);
+                    var bodybuilder = new BodyBuilder
+                    {
+                        HtmlBody = $"{message}",
+                    };
+                    var msg = new MimeMessage
+                    {
+                        Body = bodybuilder.ToMessageBody()
+                    };
+                    msg.From.Add(new MailboxAddress(_emailSettings.MailBoxName, _emailSettings.MailBoxAddress));
+                    msg.To.Add(new MailboxAddress("User", email));
+                    //message.Subject = reason == null ? "No Submitted" : reason;
+                    msg.Subject = Subject == null ? "No Submitted" : Subject;
+                    await client.SendAsync(msg);
+                    await client.DisconnectAsync(true);
+                }
+                return "Success";
+            }
+            catch (Exception)
+            {
+                return "Failed";
+            }
+        }
+        #endregion
+    }
+}
